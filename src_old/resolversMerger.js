651f720697d155fc827c38da1c7140abcc768c7b
Antoine Cormouls <antoine@boby.io>
2019-01-23 00:13:18 +0100

    Delete src old

--- src_old/resolversMerger.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const mergeGraphqlSchemas = require('merge-graphql-schemas');
-const fileLoader = mergeGraphqlSchemas.fileLoader;
-const mergeResolvers = mergeGraphqlSchemas.mergeResolvers;
-
-const resolversArray = fileLoader(path.join(__dirname, "./**/*.resolvers.*"));
-module.exports = mergeResolvers(resolversArray);
